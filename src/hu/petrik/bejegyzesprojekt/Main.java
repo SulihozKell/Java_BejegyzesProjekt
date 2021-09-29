746e13776ed6b94465d7cc486f0625dba3503c7a
Bendegúz <bendeguz@medea.hu>
2021-09-29 15:11:19 +0200

    Main-ben bejegyzések lista létrehozása 2 elemmel

--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -1,8 +1,21 @@
 package hu.petrik.bejegyzesprojekt;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Bejegyzes> bejegyzesek = new ArrayList<>();
+        bejegyzesek.add(new Bejegyzes("Valaki Neve", "Remek Tartalom"));
+        bejegyzesek.add(new Bejegyzes("Teszt Elek", "Tesztelés"));
+
+        System.out.println("Hány elemet adjak hozzá a listához? ");
+        int db = Integer.parseInt(sc.nextLine());
+
+
 
     }
 }
