033415ab0ed25906603412c22a3e49e1ddbbd81c
Bendegúz <bendeguz@medea.hu>
2021-09-29 15:04:04 +0200

    Bejegyzes Override toString()

--- src/hu/petrik/bejegyzesprojekt/Bejegyzes.java
+++ src/hu/petrik/bejegyzesprojekt/Bejegyzes.java
@@ -1,20 +1,20 @@
 package hu.petrik.bejegyzesprojekt;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Bejegyzes {
     String szerzo;
     String tartalom;
     int likeok;
-    LocalDate letrejott;
-    LocalDate szerkesztve;
+    LocalDateTime letrejott;
+    LocalDateTime szerkesztve;
 
     Bejegyzes(String szerzo, String tartalom) {
         this.szerzo = szerzo;
         this.tartalom = tartalom;
         likeok = 0;
-        letrejott = LocalDate.now();
-        szerkesztve = LocalDate.now();
+        letrejott = LocalDateTime.now();
+        szerkesztve = LocalDateTime.now();
     }
 
     public String getSzerzo() {
@@ -33,15 +33,27 @@ public class Bejegyzes {
         return likeok;
     }
 
-    public LocalDate getLetrejott() {
+    public LocalDateTime getLetrejott() {
         return letrejott;
     }
 
-    public LocalDate getSzerkesztve() {
+    public LocalDateTime getSzerkesztve() {
         return szerkesztve;
     }
 
     public void like() {
         this.likeok++;
+    }
+
+    @Override
+    public String toString() {
+        if (this.letrejott != this.szerkesztve) {
+            return this.szerzo + "-" + this.likeok + "-" + this.letrejott +
+                    "\nSzerkesztve: " + this.szerkesztve + "\n" + this.tartalom;
+        }
+        else {
+            return this.szerzo + "-" + this.likeok + "-" + this.letrejott +
+                    "\n" + this.tartalom;
+        }
     }
 }
